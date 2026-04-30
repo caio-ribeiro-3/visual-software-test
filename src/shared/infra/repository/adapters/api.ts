f73426774561d9b1202ee93b00b4e8f7dae2c116
Caio-Ribeiro-3 <devcaioribeiro@gmail.com>
2026-04-30 19:29:04 -0300

    try to reflect backend error messages by default

--- src/shared/infra/repository/adapters/api.ts
+++ src/shared/infra/repository/adapters/api.ts
@@ -5,24 +5,28 @@ import { InvalidHTTPResponse, type Repository } from "../types";
 export class ApiRepository implements Repository {
     private baseURL = import.meta.env.VITE_API_ENDPOINT as string;
 
-    private fetcher<T>({
+    private async fetcher<T>({
         body,
         entity,
         method,
     }: { entity: string; method: 'GET' | 'POST'; body?: Record<any, any> }) {
-        return fetch(`${this.baseURL}/${entity}`, {
+        const response = await fetch(`${this.baseURL}/${entity}`, {
             method,
             headers: {
                 'Content-type': 'application/json',
             },
             body: body ? JSON.stringify(body) : undefined
         })
-            .then(response => {
-                if (!response.ok) {
-                    throw new InvalidHTTPResponse();
-                }
-                return response.json() as T
-            })
+
+        if (!response.ok) {
+            try {
+                throw new Error((await response.json()).message)
+            } catch {
+                throw new InvalidHTTPResponse();
+            }
+        }
+
+        return await response.json() as T
     }
 
     list<T>(entity: string): Promise<T[]> {
